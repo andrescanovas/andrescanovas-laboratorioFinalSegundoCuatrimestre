5f1cd9e2877d1569cd09000150021dddd1444fcf
andres <andrescanovas92@gmail.com>
2021-11-19 15:52:52 -0300

    LLLL

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ApiCrud from './components/ApiCrud';
+// import AplicacionCrudApp from "./components/AplicacionCrudApp" ;
+
+
+
+function App() {
+  return (
+    <div class="hola">
+      <h1>Aplicacion con React</h1>
+
+      <ApiCrud/>
+
+      {/* <AplicacionCrudApp/> */}
+    </div>
+  );
+}
+
+export default App;
